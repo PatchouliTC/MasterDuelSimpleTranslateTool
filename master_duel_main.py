036c11b314b67f91ad97f9a5e75e28acfee8ab5d
PatchouliTC <1009609373@qq.com>
2022-01-25 00:41:34 +0800

    - fix default not generate pic cache

--- master_duel_main.py
+++ master_duel_main.py
@@ -335,7 +335,7 @@ def translate(type:int,cache:list,debug:bool=False):
         
 if __name__ == '__main__':
     cache=get_image_db_cache()
-    enable_debug=True
+    enable_debug=False
     print("shift+g翻译卡组卡片,shift+f翻译决斗中卡片,esc关闭\n请确保您已经点开了目标卡片的详细信息!!!")
     keyboard.add_hotkey('shift+g',translate,args=(1,cache,enable_debug))
     keyboard.add_hotkey('shift+f',translate,args=(2,cache,enable_debug))
