ca0c59192449f058764d08cffb6e689bb5c431e3
Jiamin Huang <hjmsens@gmail.com>
2022-02-05 13:00:03 -0800

    修改按钮样式，重构代码。

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
-# front for the texts
+# font for the texts
+BUTTON_FONT_SIZE = 8
 CARD_NAME_SIZE = 14
 CARD_DESC_SIZE = 10
-FRONT = '微软雅黑'
+PREFERRED_FONT = "Noto Sans CJK SC Regular"
+DEFAULT_FONT = '微软雅黑'
 
 START_POS_X = 200
 START_POS_Y = 200
+
+BLUE = "#008CBA"
+RED = "#f44336"
 
 # default card pic to show when there's no match
 # ideally should be the card back
--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from doctest import master
 import tkinter as tk
+import tkinter.font as tkFont
 from PIL import ImageTk, Image
 from ctypes import windll
 
@@ -12,84 +13,99 @@ from config import *
 class App():
     def __init__(self) -> None:
         self.tk = tk.Tk()
-        self.tk.minsize(200, 200)
-        # manually resizing th window will cause th window to stop auto resizing, therefore disabled
-        self.tk.resizable(False, False)
-        self.control_frame = tk.Frame(
-            master=self.tk, width=WINDOW_WIDTH, height=50)
-        self.display_frame = tk.Frame(
-            master=self.tk, width=WINDOW_WIDTH, height=200)
-
         self.is_paused = True
         self.duel_mode = True
+        self.configure_font()
+        self.configure_control_area()
+        self.configure_display_area()
+        self.configure_window()
 
-        # button in control_frame
+    def configure_font(self):
+        if PREFERRED_FONT in tkFont.families():
+            self.font = PREFERRED_FONT
+        else:
+            self.font = DEFAULT_FONT
+    
+    def configure_control_area(self):
+        self.control_area = tk.Frame(
+            master=self.tk, width=WINDOW_WIDTH, height=50)
+        # button in control_area
         self.start_button = tk.Button(
-            master=self.control_frame,
+            master=self.control_area,
+            font=(self.font, BUTTON_FONT_SIZE),
             text="开始",
             width=10,
             height=1,
-            bg="grey",
-            fg="blue",
+            bg=BLUE,
+            fg="white",
             command=self.pause_unpause,
         )
         self.mode_button = tk.Button(
-            master=self.control_frame,
+            master=self.control_area,
+            font=(self.font, BUTTON_FONT_SIZE),
             text="切换至决斗",
             width=10,
             height=1,
-            bg="grey",
-            fg="blue",
+            bg=BLUE,
+            fg="white",
             command=self.switch_mode,
         )
         self.exit_button = tk.Button(
-            master=self.control_frame,
+            master=self.control_area,
+            font=(self.font, BUTTON_FONT_SIZE),
             text="退出",
             width=10,
             height=1,
-            bg="grey",
-            fg="blue",
+            bg=RED,
+            fg="white",
             command=self.exit,
         )
+        self.control_area.pack(fill=tk.BOTH, expand=False)
+        self.start_button.pack(side=tk.LEFT, padx=10, pady=10)
+        self.mode_button.pack(side=tk.RIGHT, padx=10, pady=10)
+        self.exit_button.place(relx=.5, rely=.5,anchor=tk.CENTER)
 
-        # contents in display_frame
+    def configure_display_area(self):
+        self.display_area = tk.Frame(
+            master=self.tk, width=WINDOW_WIDTH, height=200)
+        # contents in display_area
         self.card_pic = tk.Label(
-            master=self.display_frame,
+            master=self.display_area,
             image=None,
         )
         self.card_name = tk.Label(
-            master=self.display_frame,
-            font=(FRONT, CARD_NAME_SIZE, 'bold'),
+            master=self.display_area,
+            font=(self.font, CARD_NAME_SIZE, 'bold'),
             text="",
-            fg="#3471eb",
+            fg=BLUE,
         )
         self.card_desc = tk.Message(
-            master=self.display_frame,
-            font=(FRONT, CARD_DESC_SIZE),
+            master=self.display_area,
+            font=(self.font, CARD_DESC_SIZE),
             text="",
             justify=tk.LEFT,
             # width in piels, -20 to leave room for special characters
             width=WINDOW_WIDTH - 20
         )
-
-        self.control_frame.pack(fill=tk.BOTH, expand=False)
-        self.display_frame.pack(fill=tk.BOTH, expand=True)
-        self.start_button.pack(side=tk.LEFT, padx=10, pady=10)
-        self.mode_button.pack(side=tk.RIGHT, padx=10, pady=10)
-        self.exit_button.place(relx=.5, rely=.5,anchor=tk.CENTER)
-
+        self.display_area.pack(fill=tk.BOTH, expand=True)
         self.card_pic.pack()
         self.card_name.pack()
         self.card_desc.pack()
 
+    def configure_window(self):
+        self.tk.minsize(200, 200)
+        # manually resizing th window will cause th window to stop auto resizing, therefore disabled
+        self.tk.resizable(False, False)
         self.tk.overrideredirect(True)
-        self.configure_dragger(self.control_frame)
-        self.configure_dragger(self.display_frame)
+        self.tk.attributes("-topmost", True)
+        self.tk.geometry(f"+{START_POS_X}+{START_POS_Y}")
+        self.configure_dragger(self.control_area)
+        self.configure_dragger(self.display_area)
         self.configure_dragger(self.card_pic)
         self.configure_dragger(self.card_name)
         self.configure_dragger(self.card_desc)
 
-    def update_display_frame(self, last_card):
+    def update_display_area(self, last_card):
         current_card = search.get_card_No()
         if current_card and current_card != last_card:
             last_card = current_card
@@ -97,7 +113,7 @@ class App():
             card_number = "83764718" if current_card == "83764719" else current_card
             self.load_card(card_number, True)
         self.tk.after(UPDATE_INTERVAL,
-                      lambda: self.update_display_frame(last_card))
+                      lambda: self.update_display_area(last_card))
     
     def load_card(self, card_number, show_card_desc):
         try:
@@ -155,8 +171,7 @@ class App():
 
     def start(self):
         search.start()
-        self.tk.after(UPDATE_INTERVAL, self.update_display_frame(DEFAULT_PIC))
-        self.tk.geometry(f"+{START_POS_X}+{START_POS_Y}")
+        self.tk.after(UPDATE_INTERVAL, self.update_display_area(DEFAULT_PIC))
         self.load_card(DEFAULT_PIC, False)
         self.tk.mainloop()
 
